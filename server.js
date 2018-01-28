276af0661a262b3295be1419c80dda9428d956d9
Difo0505 <soprano1993@live.fr>
2018-01-28 23:44:51 +0100

    port numer fix try

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
